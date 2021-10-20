539016df63eb06454f27f598056496e932f9f414
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2021-10-20 08:54:04 -0300

    Swagger API documentation created

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,10 +2,14 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from '../../../routes/index';
 import "../typeorm";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from '../../documentation/swagger.json';
 
 const app = express()
 
 app.use(routes)
+
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.listen(3333, () => {
     console.log('Working on port 3333')
